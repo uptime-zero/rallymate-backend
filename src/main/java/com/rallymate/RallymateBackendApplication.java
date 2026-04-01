80e812460fdc600c34d0f6c3f3bbf0917e787370
uptime-zero <dereckjun2173@gmail.com>
2026-04-01 15:29:03 +0900

    Initial commit

---
+++ src/main/java/com/rallymate/RallymateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rallymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallymateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RallymateBackendApplication.class, args);
+    }
+
+}
